8064c5e0a8720fc7fa06590253ea6b9055488352
Hans <sascha.vanrhijn@gmx.ch>
2025-12-10 19:38:56 +0100

    Initial commit

--- server.js
+++ server.js
@@ -63,8 +63,6 @@ db.getConnection()
 // ----------------------------------------------------------
 //  RESEND EMAIL SENDER
 // ----------------------------------------------------------
-
-const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendLicenseEmail(to, key, expires) {
